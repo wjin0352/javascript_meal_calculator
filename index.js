f5a9c890736717e74161bb1e7be0f00f586b56d4
won <wjin0352@gmail.com>
2016-07-04 16:09:59 -0400

    init cmmt

---
+++ index.js
@@ -0,0 +1,40 @@
+// requirements Create diner objects which represent a single diner.
+// Add dishes to a diner's meal
+// Total up the cost of all of the diners' meals
+// Add a fixed tax percentage to the total bill
+// Add a percentage tip to the total bill
+// Split the bill fairly among the diners
+// Each diner should pay the tax on their own food
+// Each diner should pay an equal share of the tip
+// If you choose to round the amounts, you may notice that the sum of the amounts does not equal the total bill amount anymore. Don't worry about that, or distribute the discrepancy in a fair way for an extra challenge.
+// Print out a total bill
+// Print a breakdown for what each diner owes
+
+var diners = [];
+
+var Diner = function(name, dishes) {
+  this.name = name;
+  this.dishes = dishes;
+  // this.tax
+
+};
+
+Diner.prototype.taxes = function() {
+
+};
+
+Diner.prototype.tips = function() {
+
+};
+
+Diner.prototype.total = function() {
+
+};
+
+Diner.prototype.print_bill = function() {
+
+};
+
+$(document).ready(function() {
+
+});
